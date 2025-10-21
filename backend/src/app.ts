8693d3ac569f4b56e3a6f26e445ed2ecaf971850
Kir.S <kir.sforit@gmail.com>
2025-10-21 13:21:08 +0300

    initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { errorHandler } from './middlewares/error-handler';
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use();
+
+// Global error handler (should be after routes)
+app.use(errorHandler);
+
+export default app;
