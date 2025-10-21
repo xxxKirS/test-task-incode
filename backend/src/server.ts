d6b9119faf225a7e4c8f71afab84b76997016b1e
Kir.S <kir.sforit@gmail.com>
2025-10-21 13:53:07 +0300

    feat: connect db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import config from './config/config';
+import { connectDB } from './config/db';
 
 app.listen(config.port, () => {
+  connectDB();
   console.log(`Server running on port ${config.port}`);
 });
