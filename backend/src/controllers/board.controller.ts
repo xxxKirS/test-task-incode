57b7d4a4a6819817a70e65dbd037b22c59ff86ae
Kir.S <kir.sforit@gmail.com>
2025-10-21 14:20:23 +0300

    feat: board controller

---
+++ backend/src/controllers/board.controller.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from 'express';
+import Board from '../models/board.model';
+import Card from '../models/card.model';
+import { customAlphabet } from 'nanoid';
+
+export async function createBoard(req: Request, res: Response) {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const board = await Board.create({
+      name,
+      hashId: customAlphabet('1234567890', 8),
+    });
+    res.status(201).json(board);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function getAllBoards(req: Request, res: Response) {
+  try {
+    const boards = await Board.find();
+    res.status(200).json(boards);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function getBoardById(req: Request, res: Response) {
+  try {
+    const board = await Board.findById(req.params.id);
+    if (!board) {
+      return res.status(404).json({ error: 'Board not found' });
+    }
+    res.status(200).json(board);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function updateBoard(req: Request, res: Response) {
+  try {
+    const board = await Board.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!board) {
+      return res.status(404).json({ error: 'Board not found' });
+    }
+    res.status(200).json(board);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+export async function deleteBoard(req: Request, res: Response) {
+  try {
+    const board = await Board.findByIdAndDelete(req.params.id);
+    if (!board) {
+      return res.status(404).json({ error: 'Board not found' });
+    }
+
+    await Card.deleteMany({ boardId: req.params.id });
+
+    res.status(200).json({ message: 'Board deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
