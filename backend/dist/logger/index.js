f3891d28c2bae0de4a3eee3c62da484ba82b97ef
Kir.S <kir.sforit@gmail.com>
2025-10-24 18:52:27 +0300

    fit: remove dist & node_modules

--- backend/dist/logger/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-exports.logger = {
-    error: (message, error) => {
-        console.error(`${message}:`, error);
-    },
-};
